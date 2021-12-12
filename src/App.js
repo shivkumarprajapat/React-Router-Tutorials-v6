eeceede9dc7b7c8b6d2a2b578a4787b75bd48d40
shivkumarprajapat <shivkumarprajapat28@gmail.com>
2021-12-12 20:57:23 +0530

    links added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import './App.css';
+import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import { Home } from './components/Home'
 import { About } from './components/About'
+import { Navbar } from './components/Navbar'
+
 function App() {
   return (
     <div className='App'>
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='about' element={<About />} />
